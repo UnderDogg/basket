3173001b0feb5f73fb81ae414d50c81af93c72d4
wnowicki <wonowicki@gmail.com>
2015-06-30 22:02:35 +0100

    after.sh

--- after.sh
+++ after.sh
@@ -4,5 +4,8 @@ echo "============================================"
 echo " Homestead - PROVISIONING"
 echo "============================================"
 
+echo "# Migrations"
 cd /var/www/basket/
 php artisan migrate
+echo "# Seeders"
+php artisan db:seed --class=DevSeeder
