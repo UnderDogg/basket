26b6058be8ff398676d1ddfdab51370a1852b27b
WNowicki <wojciech.nowicki@paybreak.com>
2015-06-30 12:52:05 +0100

    Added zizaco/entrust

--- config/app.php
+++ config/app.php
@@ -144,6 +144,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        'Zizaco\Entrust\EntrustServiceProvider',
+
     ],
 
     /*
@@ -191,7 +193,7 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
-
+        'Entrust' => 'Zizaco\Entrust\EntrustFacade',
     ],
 
 ];
