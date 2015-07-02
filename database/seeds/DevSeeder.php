98f580ea6894e537c0a75e0add121d855ae3a77c
WNowicki <wojciech.nowicki@paybreak.com>
2015-07-02 16:31:38 +0100

    Fixed seeder

--- database/seeds/DevSeeder.php
+++ database/seeds/DevSeeder.php
@@ -14,10 +14,19 @@ class DevSeeder extends Seeder
     {
         Model::unguard();
 
+        DB::insert('INSERT INTO merchants (id, name, token, created_at, updated_at) VALUES (?, ?, ?, ?, ?)', [
+            1,
+            'Test Merchant',
+            'mytoken',
+            time(),
+            time(),
+        ]);
+
         $user = new \App\User();
         $user->name = 'Dev';
         $user->email = 'dev@paybreak.com';
         $user->password = bcrypt('password');
+        $user->merchant_id = 1;
         $user->save();
 
         $reportRole = \App\Role::where('name', '=', 'report')->first();
