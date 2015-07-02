1817130361bda6fa4bb9f8e03c363e8a3b776988
wnowicki <wonowicki@gmail.com>
2015-07-02 22:58:08 +0100

    Added exception

---
+++ app/Exceptions/Exception.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * This file is part of the PayBreak/basket package.
+ *
+ * (c) PayBreak <dev@paybreak.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Exceptions;
+
+/**
+ * General Exception
+ *
+ * @author WN
+ * @package App\Exceptions
+ */
+class Exception extends \Exception
+{
+}
