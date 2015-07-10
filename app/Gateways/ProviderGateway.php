8f409310e150f8874e6e2622de6adb8a3c714951
WNowicki <wojciech.nowicki@paybreak.com>
2015-07-10 16:04:44 +0100

    ProviderGateway

---
+++ app/Gateways/ProviderGateway.php
@@ -0,0 +1,80 @@
+<?php
+/*
+ * This file is part of the PayBreak/basket package.
+ *
+ * (c) PayBreak <dev@paybreak.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Gateways;
+
+use Psr\Http\Message\ResponseInterface;
+use WNowicki\Generic\ApiClient\AbstractApiClient;
+use WNowicki\Generic\ApiClient\BadResponseException;
+use WNowicki\Generic\ApiClient\ErrorResponseException;
+
+/**
+ * Class ProviderGateway
+ *
+ * @author WN
+ * @package App\Gateways
+ */
+class ProviderGateway extends AbstractApiClient
+{
+    /**
+     * @author WN
+     * @param $baseUrl
+     * @param $token
+     * @return ProviderGateway
+     */
+    public static function make($baseUrl, $token = '')
+    {
+        return new self(
+            [
+                'base_uri' => $baseUrl,
+                'headers'  => ['MerchantToken:' . $token],
+            ]
+        );
+    }
+
+
+    /**
+     * @author WN
+     * @param array $body
+     * @return array
+     */
+    protected function processRequestBody(array $body)
+    {
+        return ['json' => $body];
+    }
+
+    /**
+     * @author WN
+     * @param ResponseInterface $response
+     * @return array
+     * @throws BadResponseException
+     */
+    protected function processResponse(ResponseInterface $response)
+    {
+        if ($responseBody = json_decode($response->getBody()->getContents(), true)) {
+            return $responseBody;
+        }
+        throw new BadResponseException('Response body was malformed JSON', $response->getStatusCode());
+    }
+
+    /**
+     * @author WN
+     * @param ResponseInterface $response
+     * @throws ErrorResponseException
+     */
+    protected function processErrorResponse(ResponseInterface $response)
+    {
+        if (($responseBody = json_decode($response->getBody()->getContents(), true)) &&
+            array_key_exists('message', $responseBody)
+        ) {
+            throw new ErrorResponseException($responseBody['message'], $response->getStatusCode());
+        }
+    }
+}
