59f8b0e20266e560cd0fb3362e7939d12cb4c322
Wojtek <wonowicki@gmail.com>
2015-07-22 21:46:49 +0100

    Application entities

--- app/Basket/Entities/ApplicationEntity.php
+++ app/Basket/Entities/ApplicationEntity.php
@@ -16,6 +16,30 @@ use WNowicki\Generic\AbstractEntity;
  * Application Entity
  *
  * @author WN
+ * @method $this setId(int $id)
+ * @method int|null getId()
+ * @method $this setPostedDate(string $postedDate)
+ * @method string|null getPostedDate()
+ * @method $this setCurrentStatus(string $currentStatus)
+ * @method string|null getCurrentStatus()
+ * @method $this setCustomer(Application\CustomerEntity $customer)
+ * @method Application\CustomerEntity|null getCustomer()
+ * @method $this setApplicationAddress(Application\AddressEntity $applicationAddress)
+ * @method Application\AddressEntity|null getApplicationAddress()
+ * @method $this setInstallation(string $installation)
+ * @method string|null getInstallation()
+ * @method $this setOrder(Application\OrderEntity $order)
+ * @method Application\OrderEntity|null getOrder()
+ * @method $this setProducts(Application\ProductsEntity $products)
+ * @method Application\ProductsEntity|null getProducts()
+ * @method $this setFulfilment(Application\FulfilmentEntity $fulfilment)
+ * @method Application\FulfilmentEntity|null getFulfilment()
+ * @method $this setApplicant(Application\ApplicantEntity $applicant)
+ * @method Application\ApplicantEntity|null getApplicant()
+ * @method $this setFinance(Application\FinanceEntity $finance)
+ * @method Application\FinanceEntity|null getFinance()
+ * @method $this setMetadata(array $metadata)
+ * @method array|null getMetadata()
  * @package App\Basket\Entities
  */
 class ApplicationEntity extends AbstractEntity
@@ -24,13 +48,14 @@ class ApplicationEntity extends AbstractEntity
         'id' => self::TYPE_INT,
         'posted_date' => self::TYPE_STRING,
         'current_status' => self::TYPE_STRING,
-        'customer',
-        'application_address',
+        'customer' => 'App\Basket\Entities\Application\CustomerEntity',
+        'application_address' => 'App\Basket\Entities\Application\AddressEntity',
         'installation' => self::TYPE_STRING,
-        'order',
-        'products',
-        'fulfilment',
-        'applicant',
+        'order' => 'App\Basket\Entities\Application\OrderEntity',
+        'products' => 'App\Basket\Entities\Application\ProductsEntity',
+        'fulfilment' => 'App\Basket\Entities\Application\FulfilmentEntity',
+        'applicant' => 'App\Basket\Entities\Application\ApplicantEntity',
+        'finance' => 'App\Basket\Entities\Application\FinanceEntity',
         'metadata' => self::TYPE_ARRAY,
     ];
 }
--- tests/Basket/Entities/InstallationEntityTest.php
+++ tests/Basket/Entities/InstallationEntityTest.php
@@ -10,6 +10,7 @@
 
 namespace Tests\Basket\Entities;
 
+use App\Basket\Entities\ApplicationEntity;
 use App\Basket\Entities\InstallationEntity;
 
 /**
@@ -22,6 +23,76 @@ class InstallationEntityTest extends \TestCase
 {
     public function testInstance()
     {
+        $entity = ApplicationEntity::make(json_decode('
+        {
+    "id": 123,
+    "posted_date": "2015-03-17T15:18:00Z",
+    "current_status": "converted",
+    "customer": {
+        "title": "Mr",
+        "first_name": "Fillibert",
+        "last_name": "Labingi",
+        "email_address": "fillibertlabingi+paybreak@gmail.com",
+        "phone_home": null,
+        "phone_mobile": "07700900124",
+        "postcode": "TN12 6ZZ"
+    },
+    "application_address": {
+        "abode": "Flat 2A",
+        "building_name": "",
+        "building_number": "1",
+        "street": "Newtown Walk",
+        "locality": "",
+        "town": "Walmington-on-Sea",
+        "postcode": "TN12 6ZZ"
+    },
+    "installation": "NoveltyRock",
+    "order": {
+        "reference": "NRE01234",
+        "amount": 0,
+        "description": "",
+        "validity": ""
+    },
+    "products": {
+        "group": "FF",
+        "options": [
+            "*"
+        ],
+        "default": "FF/1-3"
+    },
+    "fulfilment": {
+        "method": "collection",
+        "location": "Walmington-on-Sea Store"
+    },
+    "applicant": {
+        "title": "Mr",
+        "first_name": "Fillibert",
+        "last_name": "Labingi",
+        "date_of_birth": "1970-01-01",
+        "email_address": "fillibert.labingi@gmail.com",
+        "phone_home": null,
+        "phone_mobile": "07700900123",
+        "postcode": "TN12 6ZZ"
+    },
+    "finance": {
+        "loan_amount": 0,
+        "order_amount": 0,
+        "deposit_amount": 0,
+        "subsidy_amount": 0,
+        "settlement_net_amount": 0
+    },
+    "metadata": {
+        "you": "do",
+        "what_ever": "you",
+        "want": 2
+    }
+}
+        ', true));
+
+        echo $entity->toJson(JSON_PRETTY_PRINT);
+
+die();
+
         $entity = new InstallationEntity();
 
         $this->assertInstanceOf('WNowicki\Generic\Contracts\Entity', $entity);
