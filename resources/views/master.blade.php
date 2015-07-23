2b679669a445eafabcf29076e73fa1a58c0ba5dc
GSumisu <martin.smith@paybreak.com>
2015-07-23 16:42:29 +0100

    add message to dashboard

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -38,14 +38,13 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Basket</a>
+            <a class="navbar-brand" href="/">Basket</a>
         </div>
 
         {{--NAVIGATION BAR--}}
         <div id="navbar" class="navbar-collapse collapse">
             @if ( \Auth::check())
                 <ul class="nav navbar-nav">
-                    <li><a href="/">Home</a></li>
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Applications <span class="caret"></span></a>
                         <ul class="dropdown-menu">
