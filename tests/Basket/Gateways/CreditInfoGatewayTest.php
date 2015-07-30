ea852e99af034782f298914b1438e06d3aa317e3
Wojtek <wojciech.nowicki@paybreak.com>
2015-07-30 15:11:40 +0100

    CreditInfoGateway

---
+++ tests/Basket/Gateways/CreditInfoGatewayTest.php
@@ -0,0 +1,48 @@
+<?php
+/*
+ * This file is part of the PayBreak/basket package.
+ *
+ * (c) PayBreak <dev@paybreak.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tests\Basket\Gateways;
+
+use App\Basket\Gateways\CreditInfoGateway;
+
+/**
+ * Credit Information Test
+ *
+ * @author WN
+ * @package Tests\Basket\Gateways
+ */
+class CreditInformationTest extends \TestCase
+{
+    public function testInstance()
+    {
+        /** @var \App\Gateways\ApiClientFactory $mock */
+        $mock = $this->getMock('App\Gateways\ApiClientFactory');
+
+        $this->assertInstanceOf('App\Basket\Gateways\CreditInfoGateway', new CreditInfoGateway($mock));
+    }
+
+    public function testGetInstallation()
+    {
+        $mockApiClient = $this->getMock('App\Gateways\ProviderApiClient');
+
+        $mockApiClient->expects($this->any())->method('get')->willReturn([]);
+
+        $mock = $this->getMock('App\Gateways\ApiClientFactory');
+
+        $mock->expects($this->any())->method('makeApiClient')->willReturn($mockApiClient);
+
+        $creditInfoGateway = new CreditInfoGateway($mock);
+
+        $this->assertInternalType(
+            'array',
+            $creditInfoGateway->getCreditInfo('xxx', 123, 'yyy')
+        );
+    }
+}
