46229be72c436203577917df58421cc282f70ed6
Lee Hesselden <lee.hesselden@paybreak.com>
2015-07-31 14:20:25 +0100

    Improved create/edit user UI

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -45,46 +45,36 @@
             </div>
         </div>
 
-
         <div class="col-xs-12 col-sm-12 col-md-6">
             <div style="height: 100%;" class="panel panel-default">
                 <div class="panel-heading">
                     <h3 class="panel-title">USER LOCATIONS</h3>
                 </div>
                 <div class="panel-body panel-tight-space">
-                    <div style="padding-right:0px;" class="col-xs-6">
-                        <div class="panel rolePanel">
-                            <div class="panel-heading">
-                                <h3 class="panel-title">Applied Locations</h3>
-                                <hr class="hr-tight">
-                            </div>
-                            <div class="panel-body panel-tight-space">
-                                <div style="height: 340px;" id="permissionsAppliedHolder" class="connectedSortable col-xs-12">
-                                    @foreach ($locationsApplied as $location)
-                                        <div name="{{ $location->id }}" class="draggableItem">{{ $location->name }}</div>
-                                    @endforeach
-                                </div>
-                            </div>
-                        </div>
+                    <div class="col-xs-6">
+                        <h3 class="panel-title">Applied Locations</h3>
+                        <hr class="hr-tight">
                     </div>
-                    <div style="padding-left:0px;" class="col-xs-6">
-                        <div class="panel rolePanel">
-                            <div class="panel-heading">
-                                <h3 class="panel-title">Locations Available</h3>
-                                <hr class="hr-tight">
-                            </div>
-                            <div class="panel-body panel-tight-space">
-                                <div style="height: 340px;" id="permissionsAvailableHolder" class="connectedSortable col-xs-12">
-                                    @foreach ($locationsAvailable as $location)
-                                        <div name="{{ $location->id }}" class="draggableItem">{{ $location->name }}</div>
-                                    @endforeach
-                                </div>
-                            </div>
-                        </div>
+                    <div class="col-xs-6">
+                        <h3 class="panel-title">Locations Available</h3>
+                        <hr class="hr-tight">
+                    </div>
+                </div>
+                <div class="panel-body panel-tight-space" style="display: table; margin-bottom: 20px;">
+                    <div style="display: table-cell; float: none;" id="permissionsAppliedHolder" class="connectedSortable col-xs-6">
+                        @foreach ($locationsApplied as $location)
+                            <div name="{{ $location->id }}" class="draggableItem">{{ $location->name }}</div>
+                        @endforeach
+                    </div>
+                    <div style="display: table-cell; float: none;" id="permissionsAvailableHolder" class="connectedSortable col-xs-6">
+                        @foreach ($locationsAvailable as $location)
+                            <div name="{{ $location->id }}" class="draggableItem">{{ $location->name }}</div>
+                        @endforeach
                     </div>
                 </div>
             </div>
         </div>
+
     </div>
 
     <p>&nbsp;</p>
