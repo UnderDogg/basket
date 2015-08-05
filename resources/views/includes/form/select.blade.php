1aad456cebf6ff2363552555e7861801503360ee
Lee Hesselden <lee.hesselden@paybreak.com>
2015-08-05 15:34:20 +0100

    Fixed XSS vulnerabilities

--- resources/views/includes/form/select.blade.php
+++ resources/views/includes/form/select.blade.php
@@ -4,7 +4,7 @@
     @endforeach
 @endif
 
-<select class="filter form-control" name="{{ $field }}" value="{!! Request::only($field)[$field] !!}">
+<select class="filter form-control" name="{{ $field }}">
     <option value="">All</option>
     @if(count($object))
         @foreach(array_unique($selects) as $option)
@@ -15,8 +15,8 @@
                     name="{{ $option }}">{{ ucwords($option) }}</option>
         @endforeach
     @else()
-        <option selected="selected" name="{!! Request::only($field)[$field] !!}">
-            {!! Request::only($field)[$field] !!}
+        <option selected="selected" name="{{ Request::only($field)[$field] }}">
+            {{ Request::only($field)[$field] }}
         </option>
     @endif
 </select>
