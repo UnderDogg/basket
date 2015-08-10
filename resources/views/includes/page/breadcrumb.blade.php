2ab9d434cc05d78324ce03aeaf0fe13ea067e30a
EvanBarbour3 <EvanBarbour@hotmail.co.uk>
2015-08-10 16:01:52 +0100

    New implementation of breadcrumbs

--- resources/views/includes/page/breadcrumb.blade.php
+++ resources/views/includes/page/breadcrumb.blade.php
@@ -1,39 +1,10 @@
 <ol class="breadcrumb">
-    <li><a href="/{{ Request::segment(0) }}">Dashboard</a></li>
-    @if(Request::segment(1))
-        @if(!Request::segment(2)) {{-- */$last=true;/* --}} @else {{-- */$last=false;/* --}} @endif
-        @if($last == true) <li class="active"> @else <li><a href="/{{ Request::segment(1) }}"> @endif
-
-            @if(isset($override1)) {{ $override1 }} @else All {{ str_plural(ucwords(str_replace('-', ' ', Request::segment(1)))) }} @endif
-
-        @if($last == false) </a> @endif </li>
-    @endif
-    @if(Request::segment(2))
-        @if(!Request::segment(3)) {{-- */$last=true;/* --}} @else {{-- */$last=false;/* --}} @endif
-            @if($last == true) <li class="active"> @else <li><a href="/{{ Request::segment(1) }}/{{ Request::segment(2) }}"> @endif
-
-            @if(isset($override2)) {{ $override2 }} @else
-                @if(is_numeric(Request::segment(2)))
-                    {{ ucwords(Request::segment(1)) }} ID
-                @endif
-                {{ ucwords(Request::segment(2)) }}
-            @endif
-        @if($last == false) </a> @endif </li>
-    @endif
-    @if(Request::segment(3))
-        @if(!Request::segment(4)) {{-- */$last=true;/* --}} @else {{-- */$last=false;/* --}} @endif
-        @if($last == true) <li class="active"> @else <li><a href="/{{ Request::segment(1) }}/{{ Request::segment(2) }}/{{ Request::segment(3) }}"> @endif
-
-            @if(isset($override3)) {{ $override3 }} @else
-                {{ ucwords(Request::segment(3)) }}
-            @endif
-        @if($last == false) </a> @endif </li>
-    @endif
-    @if(Request::segment(4))
-        <li class="active">
-            @if(isset($override4)) {{ $override4 }} @else
-                @if(is_numeric(Request::segment(4))){{ ucwords(Request::segment(3)) }} ID @endif {{ Request::segment(4) }}
-            @endif
-        </li>
-    @endif
+    {{-- */$url='';/* --}}
+    <li><a href="/"{!! $crumbs[0] !!}>Dashboard</a></li>
+    @foreach($crumbs as $k => $v)
+        @if(end($crumbs) !== $v)<li><a href="{!!$url!!}">{!! ucwords(isset($over[$k])?$over[$k]:$v) !!}</a></li>
+        @else<li class="active">{!! ucwords(isset($over[$k])?$over[$k]:$v) !!}</li>
+        @endif
+        {{-- */$url.='/'.$v;/* --}}
+    @endforeach
 </ol>
