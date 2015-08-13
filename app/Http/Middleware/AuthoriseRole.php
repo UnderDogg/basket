1850678b3b079b8ad332ae6513470fc899bc082b
Wojtek <wojciech.nowicki@paybreak.com>
2015-08-13 09:36:28 +0100

    AuthoriseRole

---
+++ app/Http/Middleware/AuthoriseRole.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * This file is part of the PayBreak/basket package.
+ *
+ * (c) PayBreak <dev@paybreak.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Http\Middleware;
+
+use Auth;
+use Closure;
+
+class AuthoriseRole
+{
+    /**
+     * Run the request filter.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string  $role
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $role)
+    {
+        if (Auth::user()->hasRole($role)) {
+            return $next($request);
+        }
+
+        return redirect('/')->with('error', 'You don\'t have enough permission to access this area.');
+    }
+}
