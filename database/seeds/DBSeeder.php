890079cedfd594f55b9a380fb569f9072c11dec3
Wojtek <wojciech.nowicki@paybreak.com>
2015-08-13 11:39:08 +0100

    Set new Permissions and Roles

--- database/seeds/DBSeeder.php
+++ database/seeds/DBSeeder.php
@@ -31,26 +31,36 @@ class DBSeeder extends Seeder
         /*
          * PERMISSIONS   |  Nice Name    |  Name     |  Description
          */
-        $this->permissions[] = ['Merchants Management', 'merchants-management', 'merchants management'];
-        $this->permissions[] = ['Roles Management', 'roles-management', 'roles management'];
-        $this->permissions[] = ['User Management', 'user-management', 'user management'];
-        $this->permissions[] = ['Merchant Management', 'merchant-management', 'merchant management'];
-        $this->permissions[] = ['Read Applications', 'read-applications', 'read applications'];
-        $this->permissions[] = ['Read Settlements', 'read-settlements', 'read settlements'];
-        $this->permissions[] = ['Cancel Applications', 'cancel-applications', 'cancel applications'];
-        $this->permissions[] = ['Access In-Store Finance Page', 'access-in-store-finance-page', 'access in-store finance page'];
-        $this->permissions[] = ['Access In-Store Details', 'access-in-store-details', 'access in-store details'];
+        $this->permissions[1] = ['Merchants Management', 'merchants-management', 'merchants management'];
+        $this->permissions[2] = ['Merchants View', 'merchants-view', 'merchants show'];
+
+        $this->permissions[3] = ['Users Management', 'users-management', 'user management'];
+        $this->permissions[4] = ['Users View', 'users-view', 'user view'];
+
+        $this->permissions[5] = ['Roles Management', 'roles-management', 'roles management'];
+        $this->permissions[6] = ['Roles View', 'roles-view', 'roles view'];
+
+        $this->permissions[7] = ['Locations View', 'locations-view', 'locations view'];
+        $this->permissions[8] = ['Locations Management', 'locations-management', 'locations management'];
+
+        $this->permissions[9] = ['Applications View', 'applications-view', 'applications view'];
+        $this->permissions[10] = ['Applications Make', 'applications-make', 'applications make'];
+        $this->permissions[11] = ['Applications Fulfil', 'applications-fulfil', 'applications fulfil'];
+        $this->permissions[12] = ['Applications Cancel', 'applications-cancel', 'applications cancel'];
+        $this->permissions[13] = ['Applications Refund', 'applications-refund', 'applications refund'];
+
+        $this->permissions[14] = ['Reports View', 'reports-view', 'reports-view'];
 
         /*
          * ROLES         |  Nice Name    |  Name     |  Description
          */
-        $this->roles[] = ['Super User', 'su', 'Can do everything'];
+        $this->roles[] = ['System Administrator', 'su', 'Can do everything'];
 
         /*
          * PERMISSIONS FOR ROLE
          * RoleID = array PermissionsID
          */
-        $this->rolesPermissions[] = [1, 2, 3];
+        $this->rolesPermissions[] = array_keys($this->permissions);
 
         /*
          * USERS         |  Name        |  Email     |  Password     |  Merchant ID   |  Role ID
