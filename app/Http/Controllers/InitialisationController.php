aba3061d1ca5a2345123e079f92e4026b832afa4
Wojtek <wojciech.nowicki@paybreak.com>
2015-08-19 13:19:10 +0100

    MAP

--- app/Http/Controllers/InitialisationController.php
+++ app/Http/Controllers/InitialisationController.php
@@ -131,7 +131,7 @@ class InitialisationController extends Controller
      */
     public function chooseProduct($locationId, Request $request)
     {
-        $this->validate($request, ['amount' => 'required']);
+        $this->validate($request, ['amount' => 'required|numeric']);
 
         $location = $this->fetchLocation($locationId);
 
@@ -143,7 +143,7 @@ class InitialisationController extends Controller
             [
                 'options' => $gateway->getCreditInfo(
                     $location->installation->ext_id,
-                    $request->get('amount') * 100,
+                    floor($request->get('amount') * 100),
                     $location->installation->merchant->token
                 ),
                 'amount' => $request->get('amount') * 100,
--- resources/views/initialise/main.blade.php
+++ resources/views/initialise/main.blade.php
@@ -25,17 +25,20 @@
         @if(isset($options))
 
             @if(count($options) > 0)
-                <ul class="nav nav-tabs" role="tablist">
-                @foreach($options as $k => $group)
 
-                    @foreach($group['products'] as $l => $product)
+                @if(count($options) != 1)
+                    <ul class="nav nav-tabs" role="tablist">
+                    @foreach($options as $k => $group)
 
-                            <li role="presentation"{{ ($k == 0 && $l == 0)?' class=active':'' }}><a href="#prod-{{$product['id']}}" aria-controls="prod-{{$product['id']}}" role="tab" data-toggle="tab">{{$product['name']}}</a></li>
+                        @foreach($group['products'] as $l => $product)
+
+                                <li role="presentation"{{ ($k == 0 && $l == 0)?' class=active':'' }}><a href="#prod-{{$product['id']}}" aria-controls="prod-{{$product['id']}}" role="tab" data-toggle="tab">{{$product['name']}}</a></li>
+
+                        @endforeach
 
                     @endforeach
-
-                @endforeach
-                </ul>
+                    </ul>
+                @endif
 
                 <div class="tab-content">
                     @foreach($options as $k => $group)
@@ -127,7 +130,7 @@
 
                 @endforeach
             @else
-                <div class="alert alert-warning" role="alert">No available products for this amount!</div>
+                <div class="alert alert-warning col-md-12" role="alert">No available products for this amount!</div>
             @endif
 
         @endif
