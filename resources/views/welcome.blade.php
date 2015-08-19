6f7069ac74710121bfb30cb52c44bcfe3544d74c
Wojtek <wojciech.nowicki@paybreak.com>
2015-08-19 13:53:52 +0100

    Support details

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,23 +15,29 @@
         @endif
         <hr/>
         <h2>Support Details</h2>
-        <div class="well well-sm">
-            <h4>Customer Enquiries</h4>
-            <ul>
-                <li><strong>Tel:</strong> 03333 444 224</li>
-                <li><strong>Email:</strong> hello@paybreak.com</li>
-            </ul>
+
+        <div class="col-md-6">
+            <div>
+                <h4>Customer Enquiries</h4>
+                <ul>
+                    <li><strong>Tel:</strong> 03333 444 224</li>
+                    <li><strong>Email:</strong> hello@paybreak.com</li>
+                </ul>
+            </div>
         </div>
-        <div class="well well-sm">
-            <h4>Useful Links</h4>
-            <ul>
-                <li><a href="http://www.afforditnow.com/retailer/retailer-developer-support/" target="_blank">Retailer Developer Support</a>
-                    <ul>
-                        <li><a href="http://paybreak.github.io/retailer-integration-guide/" target="_blank">Integration Guide</a></li>
-                        <li><a href="http://www.afforditnow.com/wp-content/uploads/2015/02/afforditnow_images_v1.zip">Download</a> Promotional Assets (.zip file)</li>
-                    </ul>
-                </li>
-            </ul>
+
+        <div class="col-md-6">
+            <div>
+                <h4>Useful Links</h4>
+                <ul>
+                    <li><a href="http://www.afforditnow.com/retailer/retailer-developer-support/" target="_blank">Retailer Developer Support</a>
+                        <ul>
+                            <li><a href="http://paybreak.github.io/retailer-integration-guide/" target="_blank">Integration Guide</a></li>
+                            <li><a href="http://www.afforditnow.com/wp-content/uploads/2015/02/afforditnow_images_v1.zip">Download</a> Promotional Assets (.zip file)</li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 
