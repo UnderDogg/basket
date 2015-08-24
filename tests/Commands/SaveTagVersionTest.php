4529a4c055681e6527521aee43b1692592d7a193
Cameron Smith <cameron.smith@paybreak.com>
2015-08-24 08:58:20 +0100

    Created test case
    Created a new test case

---
+++ tests/Commands/SaveTagVersionTest.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * This file is part of the PayBreak/basket package.
+ *
+ * (c) PayBreak <dev@paybreak.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use App\User;
+use App\Http\Controllers;
+
+class SaveTagVersionTest extends TestCase
+{
+    /**
+     * @author WN
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        Artisan::call('tag:save');
+
+    }
+
+    /**
+     * @author CS
+     */
+    public function testFailure()
+    {
+        $versionFile = base_path() . '/version.json';
+        $this->assertFileExists($versionFile);
+    }
+
+}
