4ff50491d2b74d13ef59066c846db13a5ade14b2
Wojtek <wojciech.nowicki@paybreak.com>
2015-08-28 14:10:15 +0100

    Return template

--- resources/views/initialise/returnBack.blade.php
+++ resources/views/initialise/returnBack.blade.php
@@ -1,10 +1,25 @@
-@extends('main')
+@extends('master')
 
-@section('content')
+@section('page')
 
-    <div class="container">
-        <h1>Completed</h1>
-        <p>The application process has completed, please hand the device back to the Sales assistant.</p>
-
+    <body>
+    <div class="container-fluid">
+        <div class="col-md-12">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="pull-left">
+                        <a href="/">
+                            {!! HTML::image('image/ain-logo-standard-medium.svg', 'afforditNOW') !!}
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <br/>
+            <div class="col-md-offset-3 col-md-6 well">
+                <h1>Completed</h1>
+                <p>The application process has completed, please hand the device back to the Sales assistant.</p>
+            </div>
+        </div>
     </div>
+    </body>
 @endsection
