23e790d868824a11fdba9039497c1b8cc414a87c
EvanBarbour3 <EvanBarbour@hotmail.co.uk>
2015-09-25 14:01:20 +0100

    Removed reference from edit on location
    - Shouldn’t have been here as we shouldn’t be able to edit the reference

--- app/Http/Controllers/LocationsController.php
+++ app/Http/Controllers/LocationsController.php
@@ -122,11 +122,10 @@ class LocationsController extends Controller
     public function update($id, Request $request)
     {
         $this->validate($request, [
-            'reference' => 'required|regex:/^[A-Za-z0-9\-]+$/',
-            'active' => 'required',
             'name' => 'required',
             'email' => 'required:email',
             'address' => 'required',
+            'active' => 'required',
         ]);
 
         return $this->updateModel((new Location()), $id, 'location', '/locations', $request);
