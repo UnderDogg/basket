36d7421ca7d53ece4248f0aa59979c5cc23f7294
EvanBarbour3 <EvanBarbour@hotmail.co.uk>
2015-09-28 09:56:44 +0100

    Changed fv.js for flexibility

--- public/js/fv.js
+++ public/js/fv.js
@@ -3,37 +3,12 @@ if($(".form-horizontal")[0] && $(".form-control")[0]) {
     obj['framework'] = 'bootstrap';
     obj['icon'] = {valid: 'glyphicon glyphicon-ok', invalid: 'glyphicon glyphicon-remove', validating: 'glyphicon glyphicon-refresh'};
     obj['fields'] = {};
-    $('.form-control').each(function(i){
-        var name = $(this)[0].getAttribute('name');
-        obj['fields'][name] = {validators: { notEmpty: { message: 'The '+((name == 'email') ? 'email address' : name.replace(/_/g, ' '))+' field is required'}}};
-    });
     if(window['validation'] != undefined) {
-        if(validation['fields']) {
-            $.extend(true, obj['fields'], validation['fields']);
-        }
-        if(validation['remove']) {
-            for(var objectType in validation['remove']) {
-                console.log(objectType);
-                if(objectType == 'fields') {
-                    for(var property in validation['remove'][objectType]) {
-                        for(var remove in validation['remove'][objectType][property]) {
-                            for(var rem in validation['remove'][objectType][property]['validators']) {
-                                delete obj['fields'][property]['validators'][rem];
-                            }
-                        }
-                    }
-                }
-                if(objectType == 'icon') {
-                    delete obj['icon'];
-                }
-            }
-        }
+        $.extend(true, obj, validation);
     }
-
     $(document).ready(function() {
         $('.form-horizontal').formValidation(
             obj
         );
     });
-
 }
