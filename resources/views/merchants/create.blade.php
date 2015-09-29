c6a278e9806181cc16f7b8cd3611d9c39b6a292c
EvanBarbour3 <EvanBarbour@hotmail.co.uk>
2015-09-29 15:21:25 +0100

    Form fixes

--- resources/views/merchants/create.blade.php
+++ resources/views/merchants/create.blade.php
@@ -6,24 +6,22 @@
     @include('includes.page.breadcrumb')
 
     {!! Form::open(['url' => 'merchants', 'class' => 'form-horizontal']) !!}
-    <p>&nbsp;</p>
     <div class="col-xs-12">
         <div class="form-group">
-            {!! Form::label('name', 'Name: ', ['class' => 'col-sm-3 control-label']) !!}
-            <div class="col-sm-6">
+            {!! Form::label('name', 'Name: ', ['class' => 'col-sm-2 control-label']) !!}
+            <div class="col-sm-8">
                 {!! Form::text('name', null, ['class' => 'form-control']) !!}
             </div>
         </div><div class="form-group">
-            {!! Form::label('token', 'Token: ', ['class' => 'col-sm-3 control-label']) !!}
-            <div class="col-sm-6">
+            {!! Form::label('token', 'Token: ', ['class' => 'col-sm-2 control-label']) !!}
+            <div class="col-sm-8">
                 {!! Form::text('token', null, ['class' => 'form-control']) !!}
             </div>
         </div>
-    </div>
-    <p>&nbsp;</p>
-    <div class="form-group">
-        <div style="right: 15px" class="pull-right col-sm-3 col-xs-4">
-            {!! Form::submit('Create Merchant', ['class' => 'btn btn-info form-control', 'name' => 'createMerchantButton']) !!}
+        <div class="form-group">
+            <div class="col-sm-offset-2 col-sm-8">
+                {!! Form::submit('Create Merchant', ['class' => 'btn btn-info', 'name' => 'createMerchantButton']) !!}
+            </div>
         </div>
     </div>
     {!! Form::close() !!}
