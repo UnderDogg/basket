7aa03da3c801a0b0f000bc22a83abcd6e64bf475
EvanBarbour3 <EvanBarbour@hotmail.co.uk>
2015-11-17 14:49:00 +0000

    Added migration for commission

---
+++ database/migrations/2015_11_17_095705_add_commision_to_applications.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCommisionToApplications extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('applications', function ($table) {
+            $table->integer('ext_finance_commission')->after('ext_finance_subsidy');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('applications', function (Blueprint $table) {
+            $table->dropColumn('ext_finance_commission');
+        });
+    }
+}
