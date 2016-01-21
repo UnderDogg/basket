dd38f3d77907547059da2583b5047b93da43ac5c
EvanBarbour3 <EvanBarbour@hotmail.co.uk>
2016-01-21 14:23:43 +0000

    Migration

---
+++ database/migrations/2016_01_21_141827_add_converted_email_to_location.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddConvertedEmailToLocation extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('locations', function ($table) {
+            $table->boolean('converted_email')->default(TRUE);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('locations', function (Blueprint $table) {
+            $table->dropColumn('converted_email');
+        });
+    }
+}
