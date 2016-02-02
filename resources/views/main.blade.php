854de339f44be4242847020ee8905b842044d940
EvanBarbour3 <EvanBarbour@hotmail.co.uk>
2016-02-02 15:20:39 +0000

    Removed

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -151,16 +151,6 @@
 <script src="/js/main.js"></script>
 <script src={!! asset('/formvalidation/dist/js/formValidation.min.js') !!}></script>
 <script src={!! asset('/formvalidation/dist/js/framework/bootstrap.min.js') !!}></script>
-
-<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-<script>
-(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-    function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-e.src='//www.google-analytics.com/analytics.js';
-r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-ga('create','UA-XXXXX-X','auto');ga('send','pageview');
-</script>
 @yield('scripts')
 <script src={!! asset('/js/fv.js') !!}></script>
 </body>
