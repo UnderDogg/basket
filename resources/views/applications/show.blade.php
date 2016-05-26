ca1cb5b5224e93589a34e8cfb4dd968b03f2be7c
EvanBarbour3 <Evan@Evan-iMac.local>
2016-05-26 14:32:00 +0100

    Wording

--- resources/views/applications/show.blade.php
+++ resources/views/applications/show.blade.php
@@ -56,7 +56,7 @@
                                 @endif
                             @endif
 
-                            @if($applications->ext_resume_url && ($applications->ext_current_status == null || $applications->ext_current_status == 1 || $applications->ext_current_status == 3))
+                            @if($applications->ext_resume_url && ($applications->ext_current_status == null || $applications->ext_current_status == 'initialized' || $applications->ext_current_status == 'pending'))
                                 <dt>Resume URL</dt>
                                 <dd><a href="" id="return" data-clipboard-text="{{$applications->ext_resume_url}}">{{$applications->ext_resume_url}}</a></dd>
                             @endif
