e53ac0cfa6a816c10d404e681a5cfb8a8cba5a53
Evan Barbour <EvanBarbour@hotmail.co.uk>
2016-06-28 22:49:20 +0100

    Initial for Application send

--- app/Basket/Email/EmailApplicationService.php
+++ app/Basket/Email/EmailApplicationService.php
@@ -9,4 +9,24 @@ use WNowicki\Generic\Logger\PsrLoggerTrait;
 
 class EmailApplicationService
 {
+    use PsrLoggerTrait;
+
+    private $logger;
+
+    public function __construct(LoggerInterface $loggerInterface = null)
+    {
+        $this->logger = $loggerInterface;
+    }
+
+    public function sendDefaultApplicationEmail(Request $request, Application $application)
+    {
+    }
+
+    /**
+     * @return \Psr\Log\LoggerInterface|null
+     */
+    protected function getLogger()
+    {
+        return $this->logger;
+    }
 }
