1530db635fb2aa56e866f470c5d1b2097f35fcc9
Evan Barbour <EvanBarbour@hotmail.co.uk>
2016-06-29 15:25:29 +0100

    Sending of the email

--- app/Basket/Email/EmailApplicationService.php
+++ app/Basket/Email/EmailApplicationService.php
@@ -3,6 +3,7 @@
 namespace App\Basket\Email;
 
 use App\Basket\Application;
+use App\Http\Controllers\TemplatesController;
 use Illuminate\Http\Request;
 use Psr\Log\LoggerInterface;
 use WNowicki\Generic\Logger\PsrLoggerTrait;
@@ -18,8 +19,27 @@ class EmailApplicationService
         $this->logger = $loggerInterface;
     }
 
+    /**
+     * @author EB
+     * @param Request $request
+     * @param Application $application
+     * @return bool
+     */
     public function sendDefaultApplicationEmail(Request $request, Application $application)
     {
+        $template = TemplatesController::fetchDefaultTemplateForInstallation($application->installation);
+        $data = EmailTemplateEngine::formatRequestForEmail($request);
+
+        $txt = \DbView::make($template)->field('html')->with([])->render();
+
+        \Mail::send('emails.applications.blank', ['content' => $txt], function($message) use ($request) {
+            $message->to($request->get('email'))
+                ->subject($request->get('subject'));
+        });
+
+        $this->logInfo('EmailApplicationService: Application Email sent for Application[' . $application->id . ']');
+
+        return true;
     }
 
     /**
