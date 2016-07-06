d7569b32ff7dc90282c0b81506f3ef9392682b96
Evan Barbour <EvanBarbour@hotmail.co.uk>
2016-07-06 11:40:13 +0100

    Clean up

--- app/Basket/Helpers/CustomDepositHelper.php
+++ app/Basket/Helpers/CustomDepositHelper.php
@@ -1,5 +1,12 @@
 <?php
-
+/*
+ * This file is part of the PayBreak/basket package.
+ *
+ * (c) PayBreak <dev@paybreak.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace App\Basket\Helpers;
 
 /**
--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -1,11 +1,17 @@
 <?php
-
+/*
+ * This file is part of the PayBreak/basket package.
+ *
+ * (c) PayBreak <dev@paybreak.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace App\Http\Controllers;
 
 use App\Basket\Installation;
 use Illuminate\Http\Request;
 use PayBreak\Foundation\Exception;
-use PayBreak\Sdk\Gateways\CreditInfoGateway;
 use PayBreak\Sdk\Gateways\ProductGateway;
 
 /**
@@ -32,6 +38,13 @@ class AjaxController extends Controller
         $this->productGateway = $productGateway;
     }
 
+    /**
+     * @author SL
+     * @param Request $request
+     * @param $installation
+     * @param $product
+     * @return array|\Illuminate\Http\Response
+     */
     public function getCreditInformationForProduct(Request $request, $installation, $product)
     {
         try {
@@ -42,7 +55,6 @@ class AjaxController extends Controller
 
             $inst = Installation::findOrFail(['ext_id' => $installation])->first();
 
-//            echo print_r($inst);die;
             return $this->productGateway->getCreditInfo(
                 $inst->ext_id,
                 $product,
@@ -56,6 +68,5 @@ class AjaxController extends Controller
         } catch(\Exception $e) {
             return $this->apiResponseFromException($e);
         }
-//        return '{"amount_service":0,"apr":29.8,"customer_settlement_fee":2900,"deposit_amount":5000,"deposit_range":{"maximum_amount":10000,"minimum_amount":5000},"holiday":6,"initial_payment_upfront":true,"loan_amount":45000,"loan_cost":20205,"loan_repayment":65205,"offered_rate":26.4,"order_amount":50000,"payment_final":2714,"payment_regular":2717,"payment_start_iso":"2015-09-17","payment_start_nice":"Thursday 17th September 2015","payments":24,"promotional":{"customer_settlement_fee":2900,"date_end_iso":"2015-09-17","date_end_nice":"Thursday 17th September 2015","deposit_amount":5000,"loan_amount":45000,"order_amount":50000,"term":6,"total_cost":52900},"total_cost":70205,"total_repayment":70205}';
     }
 }
