b095c4196ed381aafdb5249c26eeebca7417fd60
Sean Leach <sean.leachy@icloud.com>
2016-07-14 17:27:19 +0100

    Permissions seeding

--- database/seeds/PermissionSeeder.php
+++ database/seeds/PermissionSeeder.php
@@ -6,7 +6,14 @@ use App\Role;
 use App\Permission;
 use App\User;
 
-class DBSeeder extends Seeder
+/**
+ * TODO: Short Description.
+ *
+ * Class PermissionSeeder
+ *
+ * @author SL
+ */
+class PermissionSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -29,7 +36,30 @@ class DBSeeder extends Seeder
     {
         Model::unguard();
 
+        $permission = new \App\Permission();
+        $permission->name = 'applications-merchant-payments';
+        $permission->display_name = 'Merchant Payments';
+        $permission->description = 'merchant payments';
+        $permission->save();
 
+        $roles = [
+            'manager',
+            'su',
+            'administrator'
+        ];
+
+        foreach ($roles as $roleName) {
+
+            try {
+                $role = \App\Role::where('name', '=', $roleName)->first();
+
+                $permission->roles()->attach($role->id);
+            } catch (Exception $e) {
+
+            }
+        }
+
+        $permission->save();
 
         Model::reguard();
     }
