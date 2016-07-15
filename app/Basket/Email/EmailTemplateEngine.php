9cd60d994d4235c499437bb532c5669dafa10f0f
Evan Barbour <EvanBarbour@hotmail.co.uk>
2016-07-15 09:53:22 +0100

    CS

--- app/Basket/Email/EmailTemplateEngine.php
+++ app/Basket/Email/EmailTemplateEngine.php
@@ -1,9 +1,23 @@
 <?php
+/*
+ * This file is part of the PayBreak/basket package.
+ *
+ * (c) PayBreak <dev@paybreak.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace App\Basket\Email;
 
 use Illuminate\Http\Request;
 
+/**
+ * Email Template Engine
+ *
+ * @author EB
+ * @package App\Basket\Email
+ */
 class EmailTemplateEngine
 {
     /**
