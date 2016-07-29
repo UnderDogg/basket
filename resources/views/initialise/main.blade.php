1e1ebf715d900ccc04b34d183ef6cbfaed41208e
Evan Barbour <EvanBarbour@hotmail.co.uk>
2016-07-29 14:51:42 +0100

    Button fixes

--- resources/views/initialise/main.blade.php
+++ resources/views/initialise/main.blade.php
@@ -273,16 +273,13 @@
                                     @endif
 
                                     <div class="row">
-                                        @if($bitwise->contains(8))
-                                            {{-- */$bitwise->remove(8);/* --}}
-                                        @endif
-                                        @if(count($bitwise->explode()) > 1) <div class="col-sm-6 col-xs-12">@else <div class="col-sm-12 col-xs-12">@endif
+                                        @if($bitwise->contains(2) && count($bitwise->explode()) > 1) <div class="col-sm-6 col-xs-12">@else <div class="col-sm-12 col-xs-12">@endif
                                             @if($bitwise->contains(2))
                                                 <button type="submit" class="btn btn-success btn-lg btn-block btn-bottom-margin">Continue with In-store Application</button>
                                             @endif
                                         </div>
-                                        @if(count($bitwise->explode()) > 1) <div class="col-sm-6 col-xs-12">@else <div class="col-sm-12 col-xs-12">@endif
-                                            @if($bitwise->contains(2) && count($bitwise->explode()) > 1)
+                                            @if($bitwise->contains(2) && count($bitwise->explode()) > 1) <div class="col-sm-6 col-xs-12">@else <div class="col-sm-12 col-xs-12">@endif
+                                            @if($bitwise->contains(4) || $bitwise->contains(8))
                                                 <button type="submit" class="btn btn-success btn-lg btn-block btn-bottom-margin" name="alternate" value="true">Create an Application Link</button>
                                             @endif
                                         </div>
