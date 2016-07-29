e29e395deba9261da1e139c9f83c83ae8666e046
Evan Barbour <EvanBarbour@hotmail.co.uk>
2016-07-29 11:54:31 +0100

    Partial

---
+++ database/migrations/2016_07_29_104347_email_template_configuration_defaults.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+/**
+ * Class EmailTemplateConfigurationDefaults
+ *
+ * @author EB
+ */
+class EmailTemplateConfigurationDefaults extends Migration
+{
+    private $field = 'email_configuration';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('installations', function (Blueprint $table) {
+            $table->text($this->field)->default('{}')->change();
+        });
+
+        $template = \App\Basket\Template::first();
+        $template->html = file_get_contents('doc/resources/default_email_template.blade.php');
+        $template->save();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('installations', function (Blueprint $table) {
+            $table->text($this->field)->nullable()->change();
+        });
+    }
+}
