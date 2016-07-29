114de43ab55ab49f74ce8e9f365f720591ca2959
Evan Barbour <EvanBarbour@hotmail.co.uk>
2016-07-29 13:34:09 +0100

    Bitwise fix (due to email remove)

--- resources/views/initialise/main.blade.php
+++ resources/views/initialise/main.blade.php
@@ -273,6 +273,9 @@
                                     @endif
 
                                     <div class="row">
+                                        @if($bitwise->contains(8))
+                                            {{-- */$bitwise->remove(8);/* --}}
+                                        @endif
                                         @if(count($bitwise->explode()) > 1) <div class="col-sm-6 col-xs-12">@else <div class="col-sm-12 col-xs-12">@endif
                                             @if($bitwise->contains(2))
                                                 <button type="submit" class="btn btn-success btn-lg btn-block btn-bottom-margin">Continue with In-store Application</button>
