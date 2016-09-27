9825a165593cfafb072d345c0ecc147f1be507ce
Manny <emmanuel.adeyemi@paybreak.com>
2016-09-27 17:04:45 +0100

    Removing redundant code and getting the aggregate settlement report

--- app/Http/Controllers/SettlementsController.php
+++ app/Http/Controllers/SettlementsController.php
@@ -98,19 +98,23 @@ class SettlementsController extends Controller
             $settlementReport = $this
                 ->settlementGateway
                 ->getSingleSettlementReport($this->fetchMerchantById($merchant)->token, $id);
+
+            $aggregateSettlementReport = $this
+                ->settlementGateway
+                ->getSingleAggregateSettlementReport($this->fetchMerchantById($merchant)->token, $id);
+
+            $aggregateSettlementReport['sum_net'] = array_sum(array_column($aggregateSettlementReport, 'Settlement Amount'));
         } catch (\Exception $e) {
             throw $this->redirectWithException('/', 'Failed fetching settlements', $e);
         }
 
-        $this->applySettlementAmounts($settlementReport);
-
         return View('settlements.settlement_report', [
             'settlementReport' => $settlementReport,
+            'aggregateSettlementReport' => $aggregateSettlementReport,
             'installation' => Application::where('ext_id', '=', $settlementReport['id'])->first(),
             'api_data' => $this->flattenRawReport($settlementReport),
             'export_api_filename' => 'settlement-raw-' . $settlementReport['id'] . '-'
                 . date_format(DateTime::createFromFormat('Y-m-d', $settlementReport['settlement_date']), 'Ymd'),
-            'view_data' => $this->flattenViewReport($settlementReport),
             'export_view_filename' => 'settlement-report-' . $settlementReport['id'] . '-'
                 . date_format(DateTime::createFromFormat('Y-m-d', $settlementReport['settlement_date']), 'Ymd'),
 
@@ -143,143 +147,5 @@ class SettlementsController extends Controller
             }
         }
         return $rtn;
-    }
-
-    /**
-     * @author EA
-     * @param array $report
-     * @return array
-     */
-    private function flattenViewReport(array $report)
-    {
-        $rtn = [];
-
-        foreach ($report['settlements'] as $settlement) {
-            $rtn[] = [
-                'Order Date' => date('d/m/Y', strtotime($settlement['received_date'])),
-                'Notification Date' => date('d/m/Y', strtotime($settlement['captured_date'])),
-                'Customer' => $settlement['customer_name'],
-                'Post Code' => $settlement['application_postcode'],
-                'Application ID' =>  $settlement['application'],
-                'Retailer Reference' => $settlement['order_reference'],
-                'Order Amount' => number_format($settlement['order_amount']/100, 2, '.', ''),
-                'Type' => $settlement['type'],
-                'Deposit' => number_format($settlement['deposit']/100, 2, '.', ''),
-                'Loan Amount' => number_format($settlement['loan_amount']/100, 2, '.', ''),
-                'Subsidy' => number_format($settlement['subsidy']/100, 2, '.', ''),
-                'Adjustment' => number_format($settlement['adjustment']/100, 2, '.', ''),
-                'Settlement Amount' => number_format($settlement['net']/100, 2, '.', ''),
-            ];
-        }
-
-        return $rtn;
-    }
-
-    /**
-     * Apply SettlementAmounts
-     *
-     * @author MS
-     * @param $settlementReport
-     */
-    private function applySettlementAmounts(&$settlementReport)
-    {
-        $settlementReport['sum_order_amount'] =
-        $settlementReport['sum_subsidy'] =
-        $settlementReport['sum_adjustment'] =
-        $settlementReport['sum_net'] = 0;
-
-        foreach ($settlementReport['settlements'] as &$settlement) {
-
-            $deposit = $loanAmount = $fulfilmentSubsidy = $cancellationSubsidy = $partial = $reversalOfPartial = $manualAdjustment = 0;
-            $type = '';
-
-            foreach ($settlement['transactions'] as $transaction) {
-                switch (strtolower($transaction['type'])) {
-                    case 'fulfilment':
-                        $type = 'Fulfilment';
-                        $deposit =  $settlement['deposit'];
-                        $loanAmount = $transaction['amount'] - $settlement['deposit'];
-                        break;
-                    case 'refund':
-                        $type = 'Cancellation';
-                        $deposit = -$settlement['deposit'];
-                        $loanAmount = $transaction['amount'] + $settlement['deposit'];
-                        break;
-                    case 'partial refund':
-                        $type = 'Refund';
-                        $partial = $partial + $transaction['amount'];
-                        break;
-                    case 'reversal of partial refund':
-                        $reversalOfPartial = $reversalOfPartial + $transaction['amount'];
-                        break;
-                    case 'merchant fee charged':
-                    case 'merchant commission':
-                        $fulfilmentSubsidy = $fulfilmentSubsidy + $transaction['amount'];
-                        break;
-                    case 'merchant fee refunded':
-                    case 'merchant commission refunded':
-                        $cancellationSubsidy = $cancellationSubsidy + $transaction['amount'];
-                        break;
-                    case 'cancellation fee':
-                        ($type == '' ? $type = 'Cancellation' : '');
-                        $cancellationSubsidy = $cancellationSubsidy + $transaction['amount'];
-                        break;
-                    case 'manual adjustment':
-                        $manualAdjustment = $manualAdjustment + $transaction['amount'];
-                        break;
-                }
-            }
-
-            $settlement['type'] = $type;
-            $settlement['deposit'] = $deposit;
-            $settlement['loan_amount'] = $loanAmount;
-
-            $settlement['subsidy'] = $this->getSettlementSubsidy($type,$fulfilmentSubsidy,$cancellationSubsidy);
-            $settlement['adjustment'] = $this->getSettlementAdjustment($type,$partial,$reversalOfPartial,$manualAdjustment);
-
-            $settlement['net'] = $settlement['deposit'] +  $settlement['loan_amount'] + $settlement['subsidy'] + $settlement['adjustment'];
-            $settlementReport['sum_net'] = $settlementReport['sum_net'] + $settlement['net'];
-        }
-    }
-
-    /**
-     * @author EA
-     * @param string $type
-     * @param int $partial
-     * @param int $reversalOfPartial
-     * @param int $manualAdjustment
-     * @return int
-     */
-    private function getSettlementAdjustment($type, $partial, $reversalOfPartial, $manualAdjustment)
-    {
-        if ($type == 'Refund') {
-            return  $partial + $manualAdjustment;
-        }
-
-        if ($type == 'Cancellation') {
-            return $manualAdjustment + $reversalOfPartial;
-        }
-
-        return (int)$manualAdjustment;
-    }
-
-    /**
-     * @author EA
-     * @param string $type
-     * @param int $fulfilmentSubsidy
-     * @param int $cancellationSubsidy
-     * @return int
-     */
-    private function getSettlementSubsidy($type, $fulfilmentSubsidy, $cancellationSubsidy)
-    {
-        if ($type == 'Fulfilment') {
-            return  (int)$fulfilmentSubsidy;
-        }
-
-        if ($type == 'Cancellation') {
-            return (int)$cancellationSubsidy;
-        }
-
-        return  0;
     }
 }
