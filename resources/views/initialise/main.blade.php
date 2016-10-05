14d6744e57059cb85bde0314bb3a385ccec7f2ed
Evan Barbour <EvanBarbour@hotmail.co.uk>
2016-10-05 10:45:10 +0100

    Added decimal support

--- resources/views/initialise/main.blade.php
+++ resources/views/initialise/main.blade.php
@@ -34,7 +34,7 @@
 
                 <div class="input-group">
                     <div class="input-group-addon">&pound;</div>
-                    {!! Form::text('ui_amount', isset($amount)?number_format($amount/100,2,'.',''):null, ['class' => 'form-control input-lg', 'maxlength' => 10, 'data-fv-digits' => 'true']) !!}
+                    {!! Form::text('ui_amount', isset($amount)?number_format($amount/100,2,'.',''):null, ['class' => 'form-control input-lg', 'maxlength' => 10, 'data-fv-numeric' => 'true']) !!}
                     {!! Form::hidden('amount', isset($amount)?number_format($amount/100,2,'.',''):null, ['class' => 'form-control input-lg', 'maxlength' => 10]) !!}
                 </div>
 
