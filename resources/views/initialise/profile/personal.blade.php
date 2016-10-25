dd2b8984a3debe1c98ff1d76f2df486b6174f453
Evan Barbour <EvanBarbour@hotmail.co.uk>
2016-10-25 21:06:51 +0100

    Personal page to accordion

--- resources/views/initialise/profile/personal.blade.php
+++ resources/views/initialise/profile/personal.blade.php
@@ -1,73 +1,79 @@
-<div class="col-sm-offset-2 col-sm-8">
-    <h2>Personal <small>(Mandatory)</small><span class="personal-status"></span></h2>
-    <hr/>
-</div>
-<div class="col-sm-12">
-    {!! Form::open(['url' => '/locations/' . $location->id . '/profile', 'class' => 'form-horizontal', 'method' => 'POST', 'id' => 'personal']) !!}
-        {!! Form::hidden('reference', isset($reference) ? $reference : null) !!}
-        <div class="form-group">
-            {!! Form::label('title', 'Title', ['class' => 'col-sm-2 control-label text-right', 'data-fv-notempty' => 'true']) !!}
-            <div class="col-sm-8">
-                <select class="form-control col-xs-12" name="title" data-fv-notempty="true" data-fv-notempty-message="Please select a title">
-                    <option value="">Please select&hellip;</option>
-                    <option value="Mr">Mr</option>
-                    <option value="Mrs">Mrs</option>
-                    <option value="Miss">Miss</option>
-                    <option value="Ms">Ms</option>
-                </select>
+<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+    <div class="panel panel-default">
+        <div class="panel-heading" role="tab" id="headingPersonal">
+            <h4 class="panel-title">
+                <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapsePersonal" aria-expanded="true" aria-controls="collapseOne">
+                    Personal <small>(Mandatory)</small><span class="personal-status"></span>
+                </a>
+            </h4>
+        </div>
+        <div id="collapsePersonal" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingPersonal">
+            <div class="panel-body">
+                {!! Form::open(['url' => '/locations/' . $location->id . '/profile', 'class' => 'form-horizontal', 'method' => 'POST', 'id' => 'personal']) !!}
+                    {!! Form::hidden('reference', isset($reference) ? $reference : null) !!}
+                    <div class="form-group">
+                        {!! Form::label('title', 'Title', ['class' => 'col-sm-2 control-label text-right', 'data-fv-notempty' => 'true']) !!}
+                        <div class="col-sm-8">
+                            <select class="form-control col-xs-12" name="title" data-fv-notempty="true" data-fv-notempty-message="Please select a title">
+                                <option value="">Please select&hellip;</option>
+                                <option value="Mr">Mr</option>
+                                <option value="Mrs">Mrs</option>
+                                <option value="Miss">Miss</option>
+                                <option value="Ms">Ms</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        {!! Form::label('first_name', 'First Name', ['class' => 'col-sm-2 control-label']) !!}
+                        <div class="col-sm-8">
+                            {!! Form::text('first_name', isset($first_name) ? $first_name : null, ['class' => 'form-control', 'data-fv-notempty' => 'true', 'data-fv-notempty-message' => 'Please enter a first name', 'maxlength' => 50]) !!}
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        {!! Form::label('last_name', 'Last Name', ['class' => 'col-sm-2 control-label']) !!}
+                        <div class="col-sm-8">
+                            {!! Form::text('last_name', isset($last_name) ? $last_name : null, ['class' => 'form-control', 'data-fv-notempty' => 'true', 'data-fv-notempty-message' => 'Please enter a last name', 'maxlength' => 50]) !!}
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        {!! Form::label('date_of_birth', 'Date of Birth', ['class' => 'col-sm-2 control-label']) !!}
+                        <div class="col-sm-8">
+                            {!! Form::text('date_of_birth', isset($date_of_birth) ? $date_of_birth : null, ['class' => 'form-control', 'data-fv-notempty' => 'true', 'data-fv-notempty-message' => 'Please enter a date of birth', 'data-fv-date' => 'true', 'data-fv-date-format' => 'YYYY-MM-DD', 'data-fv-date-message' => 'Please enter a valid date in the following format: YYYY-MM-DD', 'maxlength' => 10]) !!}
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        {!! Form::label('phone_mobile', 'Mobile Phone', ['class' => 'col-sm-2 control-label text-right']) !!}
+                        <div class="col-sm-8">
+                            {!! Form::text('phone_mobile', isset($phone_mobile) ? $phone_mobile : null, ['class' => 'form-control col-xs-12', 'data-fv-phone' => 'true', 'data-fv-phone-country' => 'GB', 'maxlength' => 11]) !!}
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        {!! Form::label('phone_home', 'Home Phone', ['class' => 'col-sm-2 control-label text-right']) !!}
+                        <div class="col-sm-8">
+                            {!! Form::text('phone_home', isset($phone_home) ? $phone_home : null, ['class' => 'form-control col-xs-12', 'data-fv-phone' => 'true', 'data-fv-phone-country' => 'GB', 'maxlength' => 11]) !!}
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        {!! Form::label('martial_status', 'Martial Status', ['class' => 'col-sm-2 control-label text-right']) !!}
+                        <div class="col-sm-8">
+                            <select class="form-control col-xs-12" name="martial_status" data-fv-notempty-message="Please select a martial status" data-fv-notempty="true">
+                                <option value="">Please select&hellip;</option>
+                                @foreach ($martialStatuses as $status)
+                                    <option value="{!!$status['id']!!}">{!!$status['description']!!}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+                    {!! Form::token() !!}
+                    <div class="form-group">
+                        <div class="col-sm-offset-2 col-sm-8">
+                            {!! Form::submit('Save Personal Information', ['class' => 'btn btn-info', 'name' => 'savePersonal']) !!}
+                        </div>
+                    </div>
+                {!! Form::close() !!}
             </div>
         </div>
-        <div class="form-group">
-            {!! Form::label('first_name', 'First Name', ['class' => 'col-sm-2 control-label']) !!}
-            <div class="col-sm-8">
-                {!! Form::text('first_name', isset($first_name) ? $first_name : null, ['class' => 'form-control', 'data-fv-notempty' => 'true', 'data-fv-notempty-message' => 'Please enter a first name', 'maxlength' => 50]) !!}
-            </div>
-        </div>
-        <div class="form-group">
-            {!! Form::label('last_name', 'Last Name', ['class' => 'col-sm-2 control-label']) !!}
-            <div class="col-sm-8">
-                {!! Form::text('last_name', isset($last_name) ? $last_name : null, ['class' => 'form-control', 'data-fv-notempty' => 'true', 'data-fv-notempty-message' => 'Please enter a last name', 'maxlength' => 50]) !!}
-            </div>
-        </div>
-        <div class="form-group">
-            {!! Form::label('date_of_birth', 'Date of Birth', ['class' => 'col-sm-2 control-label']) !!}
-            <div class="col-sm-8">
-                {!! Form::text('date_of_birth', isset($date_of_birth) ? $date_of_birth : null, ['class' => 'form-control', 'data-fv-notempty' => 'true', 'data-fv-notempty-message' => 'Please enter a date of birth', 'data-fv-date' => 'true', 'data-fv-date-format' => 'YYYY-MM-DD', 'data-fv-date-message' => 'Please enter a valid date in the following format: YYYY-MM-DD', 'maxlength' => 10]) !!}
-            </div>
-        </div>
-        <div class="form-group">
-            {!! Form::label('phone_mobile', 'Mobile Phone', ['class' => 'col-sm-2 control-label text-right']) !!}
-            <div class="col-sm-8">
-                {!! Form::text('phone_mobile', isset($phone_mobile) ? $phone_mobile : null, ['class' => 'form-control col-xs-12', 'data-fv-phone' => 'true', 'data-fv-phone-country' => 'GB', 'maxlength' => 11]) !!}
-            </div>
-        </div>
-        <div class="form-group">
-            {!! Form::label('phone_home', 'Home Phone', ['class' => 'col-sm-2 control-label text-right']) !!}
-            <div class="col-sm-8">
-                {!! Form::text('phone_home', isset($phone_home) ? $phone_home : null, ['class' => 'form-control col-xs-12', 'data-fv-phone' => 'true', 'data-fv-phone-country' => 'GB', 'maxlength' => 11]) !!}
-            </div>
-        </div>
-        <div class="form-group">
-            {!! Form::label('martial_status', 'Martial Status', ['class' => 'col-sm-2 control-label text-right']) !!}
-            <div class="col-sm-8">
-                <select class="form-control col-xs-12"
-                        name="martial_status"
-                        data-fv-notempty-message="Please select a martial status"
-                        data-fv-notempty = "true">
-                    <option value="">Please select&hellip;</option>
-                    @foreach ($martialStatuses as $status)
-                        <option value="{!!$status['id']!!}">{!!$status['description']!!}</option>
-                    @endforeach
-                </select>
-            </div>
-        </div>
-        {!! Form::token() !!}
-        <div class="form-group">
-            <div class="col-sm-offset-2 col-sm-8">
-                {!! Form::submit('Save Personal Information', ['class' => 'btn btn-info', 'name' => 'savePersonal']) !!}
-            </div>
-        </div>
-    </form>
+    </div>
 </div>
 
 @if(isset($validation) && $validation == true)
