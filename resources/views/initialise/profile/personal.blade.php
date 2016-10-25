d04bd5d3fa54d0c77c71144d44d5989dace61c86
Evan Barbour <EvanBarbour@hotmail.co.uk>
2016-10-25 12:38:05 +0100

    Submit to route, and new messages for validation

--- resources/views/initialise/profile/personal.blade.php
+++ resources/views/initialise/profile/personal.blade.php
@@ -3,13 +3,13 @@
     <hr/>
 </div>
 <div class="col-sm-12">
-    <form class="form-horizontal" id="personal" method="POST">
+    {!! Form::open(['url' => '/locations/' . $location->id . '/profile', 'class' => 'form-horizontal', 'method' => 'POST', 'id' => 'personal']) !!}
         {!! Form::hidden('reference', isset($reference) ? $reference : null) !!}
         <div class="form-group">
-            {!! Form::label('title', 'Title', ['class' => 'col-sm-2 control-label text-right']) !!}
+            {!! Form::label('title', 'Title', ['class' => 'col-sm-2 control-label text-right', 'data-fv-notempty' => 'true']) !!}
             <div class="col-sm-8">
                 <select class="form-control col-xs-12" name="title" data-fv-notempty="true" data-fv-notempty-message="Please select a title">
-                    <option>Please select&hellip;</option>
+                    <option value="">Please select&hellip;</option>
                     <option value="Mr">Mr</option>
                     <option value="Mrs">Mrs</option>
                     <option value="Miss">Miss</option>
@@ -20,19 +20,19 @@
         <div class="form-group">
             {!! Form::label('first_name', 'First Name', ['class' => 'col-sm-2 control-label']) !!}
             <div class="col-sm-8">
-                {!! Form::text('first_name', isset($first_name) ? $first_name : null, ['class' => 'form-control', 'data-fv-notempty' => 'true', 'maxlength' => 50]) !!}
+                {!! Form::text('first_name', isset($first_name) ? $first_name : null, ['class' => 'form-control', 'data-fv-notempty' => 'true', 'data-fv-notempty-message' => 'Please enter a first name', 'maxlength' => 50]) !!}
             </div>
         </div>
         <div class="form-group">
             {!! Form::label('last_name', 'Last Name', ['class' => 'col-sm-2 control-label']) !!}
             <div class="col-sm-8">
-                {!! Form::text('last_name', isset($last_name) ? $last_name : null, ['class' => 'form-control', 'data-fv-notempty' => 'true', 'maxlength' => 50]) !!}
+                {!! Form::text('last_name', isset($last_name) ? $last_name : null, ['class' => 'form-control', 'data-fv-notempty' => 'true', 'data-fv-notempty-message' => 'Please enter a last name', 'maxlength' => 50]) !!}
             </div>
         </div>
         <div class="form-group">
             {!! Form::label('date_of_birth', 'Date of Birth', ['class' => 'col-sm-2 control-label']) !!}
             <div class="col-sm-8">
-                {!! Form::text('date_of_birth', isset($date_of_birth) ? $date_of_birth : null, ['class' => 'form-control', 'data-fv-notempty' => 'true', 'data-fv-date' => 'true', 'data-fv-date-format' => 'YYYY-MM-DD', 'data-fv-date-message' => 'Please enter a valid date in the following format: YYYY-MM-DD', 'maxlength' => 10]) !!}
+                {!! Form::text('date_of_birth', isset($date_of_birth) ? $date_of_birth : null, ['class' => 'form-control', 'data-fv-notempty' => 'true', 'data-fv-notempty-message' => 'Please enter a date of birth', 'data-fv-date' => 'true', 'data-fv-date-format' => 'YYYY-MM-DD', 'data-fv-date-message' => 'Please enter a valid date in the following format: YYYY-MM-DD', 'maxlength' => 10]) !!}
             </div>
         </div>
         <div class="form-group">
@@ -64,7 +64,7 @@
         {!! Form::token() !!}
         <div class="form-group">
             <div class="col-sm-offset-2 col-sm-8">
-                <a class="btn btn-info" data-target="save" data-source="ajax">Save Personal Information</a>
+                {!! Form::submit('Save Personal Information', ['class' => 'btn btn-info', 'name' => 'savePersonal']) !!}
             </div>
         </div>
     </form>
