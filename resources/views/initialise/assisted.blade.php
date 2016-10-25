ed7be1e29aed6fa9d0678a977c7ec1a17d1f2089
Evan Barbour <EvanBarbour@hotmail.co.uk>
2016-10-25 18:09:02 +0100

    Front end changes

--- resources/views/initialise/assisted.blade.php
+++ resources/views/initialise/assisted.blade.php
@@ -24,45 +24,59 @@
                 </div>
             </div>
             <br/>
-            <h1>Additional Information</h1>
-            <p>You must provide your reference and an email address to proceed</p>
-            <br/>
-            <div class="row alternate-full-form">
-                <div class="col-xs-12">
-                    {!! Form::open(['action' => ['InitialisationController@performAssisted', $location->id], 'class' => 'form-horizontal', 'method' => 'POST']) !!}
-                    <div class="form-group">
-                        {!! Form::label('reference', 'Reference', ['class' => 'col-sm-2 control-label text-right']) !!}
-                        <div class="col-sm-10">
-                            {!! Form::text('reference', $input['reference'], ['class' => 'form-control col-xs-12', 'data-fv-notempty' => 'true', 'data-fv-notempty-message' => 'Please enter a reference', 'maxlength' => 255]) !!}
+            <div class="col-sm-8 col-sm-offset-2">
+                <h2>New Application Link</h2>
+                <div class="alert alert-info" role="alert">
+                    <em>Amount and Finance Information goes here...</em>
+                </div>
+                <div class="panel panel-default">
+                    <div class="panel-heading"><p class="panel-title">Order Information</p></div>
+                    <div class="panel-body">
+                        <div class="col-xs-12">
+                            {!! Form::open(['action' => ['InitialisationController@performAssisted', $location->id], 'class' => 'form-horizontal', 'method' => 'POST']) !!}
+                            <div class="form-group">
+                                {!! Form::label('reference', 'Reference', ['class' => 'col-sm-2 control-label text-right']) !!}
+                                <div class="col-sm-8">
+                                    {!! Form::text('reference', $input['reference'], ['class' => 'form-control col-xs-12', 'data-fv-notempty' => 'true', 'data-fv-notempty-message' => 'Please enter a reference', 'maxlength' => 255]) !!}
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                {!! Form::label('description', 'Description', ['class' => 'col-sm-2 control-label text-right']) !!}
+                                <div class="col-sm-8">
+                                    {!! Form::text('description', 'Goods & Services', ['class' => 'form-control col-xs-12', 'data-fv-notempty' => 'true', 'data-fv-notempty-message' => 'Please enter a description for this order', 'maxlength' => 255]) !!}
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                {!! Form::label('email', 'Email', ['class' => 'col-sm-2 control-label text-right']) !!}
+                                <div class="col-sm-8">
+                                    {!! Form::email('email', null, ['class' => 'form-control col-xs-12', 'data-fv-notempty' => 'true', 'data-fv-notempty-message' => 'Please enter an email address', 'data-fv-emailaddress' => 'true', 'maxlength' => 255]) !!}
+                                </div>
+                            </div>
+
+                            <!-- Previous input values -->
+                            @foreach($input as $key => $value)
+                                @if(!is_null($value) && !in_array($key, ['reference', 'description']))
+                                    {!! Form::hidden($key, $value) !!}
+                                @endif
+                            @endforeach
+                            {!! Form::hidden('installation', $location->installation->id) !!}
+
+                            <div class="form-group">
+                                <div class="col-sm-8 col-xs-12 col-sm-offset-2">
+                                    <div class="pull-right">
+                                        <a href="/" class="btn btn-default btn-bottom-margin">Cancel</a>
+                                        {!! Form::submit('Create', ['class' => 'btn btn-success btn-bottom-margin', 'name' => 'assisted', 'value' => true]) !!}
+                                    </div>
+                                </div>
+                            </div>
+
+                            {!! Form::close() !!}
                         </div>
                     </div>
-                    <div class="form-group">
-                        {!! Form::label('email', 'Email', ['class' => 'col-sm-2 control-label text-right']) !!}
-                        <div class="col-sm-10">
-                            {!! Form::email('email', null, ['class' => 'form-control col-xs-12', 'data-fv-notempty' => 'true', 'data-fv-notempty-message' => 'Please enter an email address', 'data-fv-emailaddress' => 'true', 'maxlength' => 255]) !!}
-                        </div>
-                    </div>
-
-                    <!-- Previous input values -->
-                    @foreach($input as $key => $value)
-                        @if(!is_null($value) && !in_array($key, ['reference', 'description']))
-                            {!! Form::hidden($key, $value) !!}
-                        @endif
-                    @endforeach
-                    {!! Form::hidden('installation', $location->installation->id) !!}
-                    {!! Form::hidden('description', 'Goods & Services') !!}
-
-                    <div class="form-group">
-                        <div class="col-sm-12 col-xs-12">
-                            {!! Form::submit('Continue with Assisted Journey', ['class' => 'btn btn-success btn-lg btn-block btn-bottom-margin', 'name' => 'assisted', 'value' => true]) !!}
-                            {{--<button type="submit" class="btn btn-success btn-lg btn-block btn-bottom-margin" name="assisted" value="true">Continue with Assisted Journey</button>--}}
-                        </div>
-                    </div>
-
-                    {!! Form::close() !!}
                 </div>
             </div>
         </div>
+    </div>
     </body>
 
 @endsection
