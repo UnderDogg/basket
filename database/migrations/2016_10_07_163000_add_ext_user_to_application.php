ae727afe9d824cfdb914e779a5c954ec41394167
Evan Barbour <EvanBarbour@hotmail.co.uk>
2016-10-26 12:22:40 +0100

    Add ext_user to Application

---
+++ database/migrations/2016_10_07_163000_add_ext_user_to_application.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+/**
+ * @author EB
+ * Class AddExtUserToApplication
+ */
+class AddExtUserToApplication extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('applications', function (Blueprint $table) {
+            $table->integer('user')->nullable()->after('ext_cancellation_fee_amount');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('applications', function (Blueprint $table) {
+            $table->dropColumn('user');
+        });
+    }
+}
