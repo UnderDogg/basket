8800fe42d18582bfd0b88adfba4b3fd0ed6098c3
Simon Dawson <simon.dawson@paybreak.com>
2016-10-28 09:35:39 +0100

    Formatting

--- resources/views/initialise/profile.blade.php
+++ resources/views/initialise/profile.blade.php
@@ -35,7 +35,8 @@
                 @if(!isset($user))
                     @include('initialise.profile.personal', ['validation' => true])
                 @else
-                    <p>The following sections are optional. The customer will be required to provide any details not completed here when they click on the application link.</p>
+                    <p>The following sections are optional.</p>
+                    <p>The customer will be required to provide any details not completed here when they click on the application link.</p>
                     <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
                         @include('initialise.profile.address', ['validation' => true])
                         @include('initialise.profile.employment', ['validation' => true])
