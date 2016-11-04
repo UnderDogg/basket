454cd9a02035ee6e34a3d8f729c08d6ac8a65bdd
Evan Barbour <EvanBarbour@hotmail.co.uk>
2016-11-04 14:31:28 +0000

    More testing

---
+++ tests/Services/InstallationSynchronisationServiceTest.php
@@ -0,0 +1,116 @@
+<?php
+/*
+ * This file is part of the PayBreak/basket package.
+ *
+ * (c) PayBreak <dev@paybreak.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use App\User;
+use Illuminate\Support\Facades\Artisan;
+
+/**
+ * Installation Synchronisation Service Test
+ *
+ * @author EB
+ */
+class InstallationSynchronisationServiceTest extends TestCase
+{
+    /**
+     * @author EB
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        Artisan::call('migrate');
+        Artisan::call('db:seed', ['--class' => 'DevSeeder']);
+
+        $user = User::find(1);
+        $this->be($user);
+    }
+
+    /**
+     * Tear Down
+     *
+     * Required for using Mockery
+     *
+     * @author EB
+     */
+    public function tearDown()
+    {
+        Mockery::close();
+    }
+
+    /**
+     * @author EB
+     */
+    public function testSynchroniseInstallation()
+    {
+        $response = [
+            'return_url' => 'go.com',
+            'features' => [
+                'assisted_journey' => true,
+            ],
+        ];
+
+        $mockApiClient = $this->getMock('PayBreak\Sdk\ApiClient\ProviderApiClient');
+        $mockApiClient->expects($this->any())->method('get')->willReturn($response);
+
+        $mock = $this->getMock('PayBreak\Sdk\ApiClient\ApiClientFactoryInterface');
+        $mock->expects($this->any())->method('makeApiClient')->willReturn($mockApiClient);
+
+        $installationGateway = new \PayBreak\Sdk\Gateways\InstallationGateway($mock);
+        $service = new \App\Basket\Synchronisation\InstallationSynchronisationService($installationGateway);
+
+        $rtn = $service->synchroniseInstallation(1);
+
+        $this->assertInstanceOf(\App\Basket\Installation::class, $rtn);
+        $this->assertSame('1', $rtn->id);
+        $this->assertTrue($rtn->assisted_journey);
+        $this->assertSame($rtn->ext_return_url, 'go.com');
+    }
+
+    /**
+     * @author EB
+     */
+    public function testSynchroniseInstallationForException()
+    {
+        $mockApiClient = $this->getMock('PayBreak\Sdk\ApiClient\ProviderApiClient');
+        $mockApiClient->expects($this->any())->method('get')
+            ->willThrowException(new Exception('Synchronise Installation Failed'));
+
+        $mock = $this->getMock('PayBreak\Sdk\ApiClient\ApiClientFactoryInterface');
+        $mock->expects($this->any())->method('makeApiClient')->willReturn($mockApiClient);
+
+        $installationGateway = new \PayBreak\Sdk\Gateways\InstallationGateway($mock);
+        $service = new \App\Basket\Synchronisation\InstallationSynchronisationService($installationGateway);
+
+        $this->setExpectedException('Exception', 'Problem with get: Installation data form Provider API');
+        $service->synchroniseInstallation(1);
+    }
+
+    /**
+     * @author EB
+     */
+    public function testSynchroniseAllInstallationsForException()
+    {
+        $mockApiClient = $this->getMock('PayBreak\Sdk\ApiClient\ProviderApiClient');
+        $mockApiClient->expects($this->any())->method('get')
+            ->willThrowException(new Exception('Synchronise Installations Failed'));
+
+        $mock = $this->getMock('PayBreak\Sdk\ApiClient\ApiClientFactoryInterface');
+        $mock->expects($this->any())->method('makeApiClient')->willReturn($mockApiClient);
+
+        $installationGateway = new \PayBreak\Sdk\Gateways\InstallationGateway($mock);
+        $service = new \App\Basket\Synchronisation\InstallationSynchronisationService($installationGateway);
+
+        $this->setExpectedException(
+            'WNowicki\Generic\Exception',
+            'Problem fetching collection of [\PayBreak\Sdk\Entities\InstallationEntity] form Provider API'
+        );
+        $service->synchroniseAllInstallations(1);
+    }
+}
