41c83ffd1e5cfa9e92c8633602ddf36d4a82aeac
James Harper <jameswilliamharper@gmail.com>
2017-06-15 11:55:54 +0100

    Update middleware groups

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -32,14 +32,28 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
-        \App\Http\Middleware\EncryptCookies::class,
-        \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-        \Illuminate\Session\Middleware\StartSession::class,
-        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-        \App\Http\Middleware\VerifyCsrfToken::class,
-        \App\Http\Middleware\AvailableInstallations::class,
-        \App\Http\Middleware\Messages::class,
-        \App\Http\Middleware\Download::class,
+    ];
+
+    /**
+     * The application's route middleware groups.
+     *
+     * @var array
+     */
+    protected $middlewareGroups = [
+        'web' => [
+            \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\VerifyCsrfToken::class,
+            \App\Http\Middleware\AvailableInstallations::class,
+            \App\Http\Middleware\Messages::class,
+            \App\Http\Middleware\Download::class,
+        ],
+
+        'api' => [
+            'throttle:60,1',
+        ],
     ];
 
     /**
