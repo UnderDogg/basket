0955b203dbb9a07350565b5de95190275b9e90a4
Simon Dawson <simon.dawson@paybreak.com>
2017-06-19 15:30:56 +0100

    StyleCI fixes

--- database/migrations/2017_06_19_140749_update_created_at_updated_at_in_templates_table.php
+++ database/migrations/2017_06_19_140749_update_created_at_updated_at_in_templates_table.php
@@ -13,8 +13,8 @@ class UpdateCreatedAtUpdatedAtInTemplatesTable extends Migration
     public function up()
     {
         Schema::table('templates', function (Blueprint $table) {
-          $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'))->change();
-          $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'))->change();
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'))->change();
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'))->change();
         });
     }
 
--- database/migrations/2017_06_19_141929_update_created_at_updated_at_in_application_events_table.php
+++ database/migrations/2017_06_19_141929_update_created_at_updated_at_in_application_events_table.php
@@ -13,8 +13,8 @@ class UpdateCreatedAtUpdatedAtInApplicationEventsTable extends Migration
     public function up()
     {
         Schema::table('application_events', function (Blueprint $table) {
-          $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
-          $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
         });
     }
 
