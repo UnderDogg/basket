d49839a8c44c4afc68add875e39916e09d79a60e
Simon Dawson <simon.dawson@paybreak.com>
2017-06-19 15:28:28 +0100

    timestamp fix

---
+++ database/migrations/2017_06_19_140749_update_created_at_updated_at_in_templates_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateCreatedAtUpdatedAtInTemplatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('templates', function (Blueprint $table) {
+          $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'))->change();
+          $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'))->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('templates', function (Blueprint $table) {
+            //
+        });
+    }
+}
---
+++ database/migrations/2017_06_19_141929_update_created_at_updated_at_in_application_events_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateCreatedAtUpdatedAtInApplicationEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('application_events', function (Blueprint $table) {
+          $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+          $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('application_events', function (Blueprint $table) {
+            //
+        });
+    }
+}
