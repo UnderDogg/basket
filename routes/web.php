aacdf8a767d64cf99ee70fa4fe99bd4dab41cae4
Sean Leach <sean.leachy@icloud.com>
2017-07-26 15:25:14 +0100

    Remove simple 'show' view in favour of edit as default

--- routes/web.php
+++ routes/web.php
@@ -208,8 +208,7 @@ Route::group(['middleware' => 'auth'], function () {
     /*
      * Account
      */
-    Route::get('account', 'AccountController@show');
-    Route::get('account/edit', 'AccountController@edit');
+    Route::get('account', 'AccountController@edit');
     Route::post('account/edit', 'AccountController@update');
     Route::post('account/edit/password', 'AccountController@changePassword');
 });
